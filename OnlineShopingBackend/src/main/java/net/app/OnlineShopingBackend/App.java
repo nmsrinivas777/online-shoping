ae7f521eebdfad53de3a340a66fe3f3172e7088b
srinu y <srinu y@LAPTOP-U9VVLMLA>
2020-02-09 12:44:28 +0530

    project setup completed successfully

---
+++ OnlineShopingBackend/src/main/java/net/app/OnlineShopingBackend/App.java
@@ -0,0 +1,13 @@
+package net.app.OnlineShopingBackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
